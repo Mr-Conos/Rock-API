c2a1a9c348c2d7e6b62f6b62c861bc91be6afb7e
Mr-Conos <76495776+Mr-Conos@users.noreply.github.com>
2022-01-27 22:24:40 -0800

    Fixed Base URL

--- tests/put.py
+++ tests/put.py
@@ -2,6 +2,6 @@ import requests
 
 
 # Base Ip
-BASE = "http://127.0.0.1:5000/"           
+BASE = "https://mrconos.pythonanywhere.com/"           
 response = requests.put(BASE + "rock/" + "test", {"name": "test","desc":"test rock", "image": "none","rating": 4})
 print(response.json())
