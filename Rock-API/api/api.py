b9f894732a0112f9202196760205d26956d0709c
Conos <76495776+Mr-Conos@users.noreply.github.com>
2022-06-27 22:13:09 -0700

    Update api.py

--- Rock-API/api/api.py
+++ Rock-API/api/api.py
@@ -90,11 +90,11 @@ class Rockss(Resource):
     @marshal_with(resource_fields)
     def put(self, name):
         if not request.headers.getlist("X-Forwarded-For"):
-		ip = request.remote_addr
+	    ip = request.remote_addr
 	else:
-		ip = request.headers.getlist("X-Forwarded-For")[0]
+	    ip = request.headers.getlist("X-Forwarded-For")[0]
 	if ip not in trusted_ips:
-		abort(403)
+	    abort(403)
         idss = generate_id()
         args = rock_put_args.parse_args()
         result = RockMod.query.filter_by(name=name).first()
@@ -109,12 +109,12 @@ class Rockss(Resource):
     @marshal_with(resource_fields)
     def patch(self, name):
         if not request.headers.getlist("X-Forwarded-For"):
-		ip = request.remote_addr
+	    ip = request.remote_addr
 	else:
-		ip = request.headers.getlist("X-Forwarded-For")[0]
+	    ip = request.headers.getlist("X-Forwarded-For")[0]
 
 	if ip not in trusted_ips:
-		abort(403)
+	    abort(403)
         args = rock_update_args.parse_args()
         result = RockMod.query.filter_by(name=name).first()
         if not result:
